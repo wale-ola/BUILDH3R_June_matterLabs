38e41560523e853587a903a380cd55e9b9489c9f
wale-ola <waleo1124@gmail.com>
2024-06-13 19:35:25 -0700

    Update paymaster-transaction.ts

--- scripts/paymaster-transaction.ts
+++ scripts/paymaster-transaction.ts
@@ -6,9 +6,9 @@ import TokenArtifact from "../artifacts/TestToken";
 import ZeekMessagesArtifact from "../artifacts/ZeekSecretMessages";
 
 // Address of the ZeekMessages contract
-const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+const ZEEK_MESSAGES_CONTRACT_ADDRESS = "0xacef981ea39dbf4c7eae1cbbecbfbbfadb1f2e5d";
 // Address of the ERC20 token contract
-const TOKEN_CONTRACT_ADDRESS = "0xf8e81D47203A594245E36C48e151709F0C19fBe8"
+const TOKEN_CONTRACT_ADDRESS = "0xf033efcabe5e9beac1b4d85c68b475aaaf235cb1"
 // Message to be sent to the contract
 const NEW_MESSAGE = "This tx cost me no ETH!";
 
